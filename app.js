2d96e0b00d777f096b32b2b8737367537ac4860f
YS0448 <yogeshsingh0448@gmail.com>
2025-08-13 19:04:46 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors({
-    // origin: process.env.CLIENT_URL,  
+    origin: process.env.CLIENT_URL,  
     methods: ['GET','POST', 'PUT', 'DELETE', 'PATCH' ],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
