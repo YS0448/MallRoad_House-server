13a8c72339cbe00489989ecd31d577600e8872fd
YS0448 <yogeshsingh0448@gmail.com>
2025-08-13 16:04:17 +0530

    add api

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL,  // or whatever your frontend runs on
+    origin: process.env.CLIENT_URL,  
     methods: ['GET','POST', 'PUT', 'DELETE', 'PATCH' ],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -30,8 +30,8 @@ app.use(session({
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(routes)
-app.get('/test', (req,res) =>{        
-    res.send('Hello World');
+app.get('/', (req,res) =>{        
+    res.send('Welcone to Mall Road House API');
 })
 
 app.use(errorHandler)
